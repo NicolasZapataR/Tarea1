8482e848b7224bec4de6d173c7b1c2161c4cfcc4
Carlos-Navia <carlosa.naviab@uqvirtual.edu.co>
2026-02-10 23:15:36 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("modificaciones realizadas en dev_CarlosNavia 22:58");
+        System.out.println("modificaciones realizadas en dev_CarlosNavia cambio#2");
 
         Sytem.out.println("cambio nicolas rama carlos");
         Sytem.out.println("cambio nicolas rama  local carlos");
