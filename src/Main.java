74e637c3a569c00d419c11d7131821f820d8a25c
Carlos-Navia <carlosa.naviab@uqvirtual.edu.co>
2026-02-10 23:34:31 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("modificaciones realizadas en dev_CarlosNavia 22:49");
 
-        System.out.println("cambio de nicolas");
-
-        System.out.println("cambios carlos navia");
-        }
+        System.out.println("modificaciones realizadas nicolas 11:30");
+        System.out.println("modificaciones CarlosNavia 23:34");
+     }
     }
