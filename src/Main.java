11156ae1276b811c6b25276ef54a39facdc3c5c9
NicolasZapataR <nicolas.zapatar@uqvirtual.edu.co>
2026-02-10 23:08:38 -0500

    cambio de nicolas rama carlos

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("modificaciones realizadas en dev_CarlosNavia 22:58");
 
+        Sytem.out.println("cambio nicolas rama carlos");
 
 
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+
         }
     }
-}
+
